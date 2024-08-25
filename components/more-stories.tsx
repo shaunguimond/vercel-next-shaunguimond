c0405b4351b2dc385083c4fb0e2a8d6da20b7781
Shaun Guimond <shaun.guimond@outlook.com>
2024-08-25 15:06:06 -0300

    Removed nested CSS causing errors, added missing margins.

--- components/more-stories.tsx
+++ components/more-stories.tsx
@@ -2,7 +2,7 @@ import PostPreview from './post-preview'
 
 export default function MoreStories({ posts }) {
   return (
-    <section>
+    <section className='mx-1'>
      {/*
       <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
         More Stories
