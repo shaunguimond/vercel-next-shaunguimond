273a72e8d16a176aaba63868b3e216570f55f5c0
Shaun Guimond <shaun.guimond@outlook.com>
2024-11-25 21:50:18 -0500

    First attempt for comments with Bluesky.

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -11,11 +11,12 @@ import PostTitle from '../../components/post-title'
 import Tags from '../../components/tags'
 import { getAllPostsWithSlug, getPostAndMorePosts } from '../../lib/api'
 import { CMS_NAME } from '../../lib/constants'
+import { CommentSection } from '../../components/Bluesky/bsky-comments'
 
 // Used to generate `/posts/[slug]` posts from the Wordpress backend.
 export default function Post({ post, posts, preview }) {
   const router = useRouter()
-  const morePosts = posts?.edges
+  const morePosts = posts?.edges 
 
   if (!router.isFallback && !post?.slug) {
     return <ErrorPage statusCode={404} />
@@ -51,6 +52,10 @@ export default function Post({ post, posts, preview }) {
 
             <SectionSeparator />
             {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+
+            <SectionSeparator />
+
+            <CommentSection uri={post.extraPostInfo.blueskyPostUrl} />
           </>
         )}
     </Layout>
