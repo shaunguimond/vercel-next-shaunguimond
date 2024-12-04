33204b44a2f37bd527ef8f2f17a0496465de139f
Shaun Guimond <shaun.guimond@outlook.com>
2024-12-03 22:03:22 -0500

    Added formatting to text among other minor fixes (#6)
    * Added formatting to text among other minor fixes

--- components/Bluesky/bsky-section-recent-posts.tsx
+++ components/Bluesky/bsky-section-recent-posts.tsx
@@ -4,6 +4,8 @@ import { AppBskyFeedGetAuthorFeed } from "@atproto/api";
 import { useEffect, useState } from 'react';
 import { BSKY_AUTHOR_FEED } from "../../lib/constants";
 import { Reposted, Likes, Comments } from "../../lib/icons";
+import sanitizeHtml from 'sanitize-html';
+import React from 'react';
 
 export default function BskySectionRecentPosts() {
     const [feedData, setFeedData] = useState([]);
@@ -28,7 +30,31 @@ export default function BskySectionRecentPosts() {
                                 `${daysAgo} days ago`;
                         const isReposted = data.reason !== undefined && data.reason.$type === "app.bsky.feed.defs#reasonRepost";
                         const postLink = `https://bsky.app/profile/${data.post.author.handle}/post/${data.post.uri.split("/")[4]}`;
+                        // let sanitizedContent = sanitizeHtml(data.post.record.text, {
+                        //     allowedTags: ['b', 'i', 'em', 'strong', 'p', 'br', 'ul', 'li'], // Allowed HTML tags
+                        //     allowedAttributes: {
+                        //         'a': ['href']
+                        //     }
 
+                        // });
+
+                        // if (data?.post?.record?.facets !== undefined) {
+                        //     data?.post?.record?.facets.map((facet, index) => {
+                        //         facet.features.map((feature, index) => {
+                        //             if (feature.$type === "app.bsky.richtext.facet#link") {
+                        //                 const byteStart = facet.index.byteStart - 1;
+                        //                 const byteEnd = facet.index.byteEnd;
+                        //                 const externalLink = `<a href="${feature.uri}" target="_blank" >`;
+
+                        //                 sanitizedContent = sanitizedContent.slice(0, byteStart) + externalLink + sanitizedContent.slice(byteStart, byteEnd) + '</a>' + sanitizedContent.slice(byteEnd);
+
+                        //             }
+                        //         })
+                        //     })
+                        // }
+                        // let formattedContent = sanitizedContent.replace(/\n/g, '<br>');
+
+                        const formattedContent = getFormattedText(data.post);
                         return (
                             <article key={index} className="break-inside mb-5 shadow-small rounded-2xl h-fit bg-sg-multicolour hover:shadow-medium transition-shadow duration-200">
                                 <div className="mb-0 backdrop-blur-xl rounded-2xl">
@@ -50,7 +76,8 @@ export default function BskySectionRecentPosts() {
 
                                     {/* This should be moved to a separate component */}
                                     <div className="py-3 px-5">
-                                        <p className="text-lg">{data.post.record.text}</p>
+                                        <p className="text-lg mb-5" dangerouslySetInnerHTML={{ __html: formattedContent }}></p>
+
 
                                         {data?.post?.embed?.$type === "app.bsky.embed.recordWithMedia#view" ?
                                             data?.post?.embed?.media?.$type === "app.bsky.embed.images#view" ?
@@ -61,25 +88,28 @@ export default function BskySectionRecentPosts() {
                                                 : "" : ""
                                         }
 
+                                        {data?.post?.embed?.$type === "app.bsky.embed.record#view" ?
+                                            <ViewRecord record={data.post.embed.record} /> : ""}
+
                                         {data?.post?.embed?.$type === "app.bsky.embed.images#view" ?
                                             <ImageEmbed images={data.post.embed.images} /> : ""}
 
                                         {data?.post?.embed?.$type === "app.bsky.embed.external#view" ?
                                             <ExternalView embed={data.post.embed} /> : ""}
 
-                                    </div>
 
-                                    {/* This should be moved to a separate component */}
-                                    <div className="flex flex-row justify-between w-11/12 mx-auto pb-4">
-                                        <a target="_blank" href={postLink} className="flex flex-row hover:shadow-medium transition-shadow duration-200"><Likes height="25px" width="25px" /><span className="self-center ml-2">{data.post.likeCount}</span></a>
-                                        <a target="_blank" href={postLink} className="flex flex-row hover:shadow-medium transition-shadow duration-200"><Reposted height="25px" width="25px" /><span className="self-center ml-2">{data.post.repostCount + data.post.quoteCount}</span></a>
-                                        <a target="_blank" href={postLink} className="flex flex-row hover:shadow-medium transition-shadow duration-200"><Comments height="25px" width="25px" /><span className="self-center ml-2">{data.post.replyCount}</span></a>
-                                    </div>
+                                        {/* This should be moved to a separate component */}
+                                        <div className="flex flex-row justify-between w-11/12 mx-auto pb-4">
+                                            <a target="_blank" href={postLink} className="flex flex-row hover:shadow-medium transition-shadow duration-200"><Likes height="25px" width="25px" /><span className="self-center ml-2">{data.post.likeCount}</span></a>
+                                            <a target="_blank" href={postLink} className="flex flex-row hover:shadow-medium transition-shadow duration-200"><Reposted height="25px" width="25px" /><span className="self-center ml-2">{data.post.repostCount + data.post.quoteCount}</span></a>
+                                            <a target="_blank" href={postLink} className="flex flex-row hover:shadow-medium transition-shadow duration-200"><Comments height="25px" width="25px" /><span className="self-center ml-2">{data.post.replyCount}</span></a>
+                                        </div>
 
-                                    <div className="py-3 px-5 w-full">
-                                        <a target="_blank" href={postLink} className="bg-brand block text-center hover:shadow-medium w-full transition-shadow duration-200 text-white font-bold text-lg py-2 px-4 rounded">
-                                            View post on BlueSky
-                                        </a>
+                                        <div className="py-3 px-5 w-full">
+                                            <a target="_blank" href={postLink} className="bg-brand block text-center hover:shadow-medium w-full transition-shadow duration-200 text-white font-bold text-lg py-2 px-4 rounded">
+                                                View post on BlueSky
+                                            </a>
+                                        </div>
                                     </div>
                                 </div>
                             </article>
@@ -115,7 +145,7 @@ const getAuthorFeed = async () => {
 const ImageEmbed = ({ images }) => {
     const hasMultiImages = images.length > 1;
     return (
-        <div className={hasMultiImages ? "grid grid-cols-2 gap-2 py-3 row-auto" : "py-3"}>
+        <div className={hasMultiImages ? "grid grid-cols-2 gap-2 row-auto mb-5" : "mb-5"}>
             {images.map((image, index) => (
                 <img className="rounded-2xl w-full max-h-600 h-content object-cover" key={index} src={image.thumb} alt={`image-${index}`} />
             ))}
@@ -124,11 +154,12 @@ const ImageEmbed = ({ images }) => {
 };
 
 const ExternalView = ({ embed }) => {
+    const hasThumbnail = embed.external.thumb;
     return (
-        <div className=" rounded-2xl backdrop-blur-xl shadow-small h-fit hover:shadow-medium transition-shadow duration-200m my-2">
+        <div className=" rounded-2xl backdrop-blur-xl shadow-small h-fit hover:shadow-medium transition-shadow duration-200m my-5">
             <a target="_blank" href={embed.external.uri}>
                 <img className="rounded-t-2xl" src={embed.external.thumb} />
-                <div className="px-2 pb-3 border-x border-b rounded-b-2xl">
+                <div className={hasThumbnail ? "px-2 pb-3 border-x border-b rounded-b-2xl" : "px-2 py-2 border-x border-y rounded-2xl"}>
                     <p className="text-md font-bold">{embed.external.title}</p>
                     <p className="text-sm pb-1">{embed.external.description}</p>
                     <hr className="pb-1" />
@@ -142,7 +173,6 @@ const ExternalView = ({ embed }) => {
 const ViewRecord = ({ record }) => {
     const date = new Date(record.value.createdAt);
     const currentDate = new Date();
-    console.log(date)
     const daysAgo = Math.floor((currentDate.getTime() - date.getTime()) / (1000 * 60 * 60 * 24));
     const renderTimeFromPost = daysAgo === 0 ?
         "Today" :
@@ -150,8 +180,10 @@ const ViewRecord = ({ record }) => {
             `${daysAgo % 7} weeks ago` :
             `${daysAgo} days ago`;
     const postLink = `https://bsky.app/profile/${record.author.handle}/post/${record.uri.split("/")[4]}`;
+
+    const formattedContent = getFormattedTextForRecord(record);
     return (
-        <div className="rounded-2xl backdrop-blur-xl shadow-small h-fit hover:shadow-medium transition-shadow duration-200 border">
+        <div className="rounded-2xl backdrop-blur-xl shadow-small h-fit hover:shadow-medium transition-shadow duration-200 border mb-5">
             <a target="_blank" href={postLink} className="mb-0 backdrop-blur-xl rounded-2xl">
 
                 {/* This should be moved to a separate component */}
@@ -169,16 +201,7 @@ const ViewRecord = ({ record }) => {
                 </div>
                 {/* This should be moved to a separate component */}
                 <div className="py-3 px-5">
-                    <p className="text-lg">{record.value.text}</p>
-
-                    {/* {record?.$type === "app.bsky.embed.recordWithMedia#view" ?
-          record?.embed?.media?.$type === "app.bsky.embed.images#view" ?
-            <>
-              <ImageEmbed images={data.post.embed.media.images} />
-              <ViewRecord record={data.post.embed.record.record} />
-            </>
-            : "" : ""
-        } */}
+                    <p className="text-lg" dangerouslySetInnerHTML={{ __html: formattedContent }}></p>
 
                     {record?.embeds[0].$type === "app.bsky.embed.images#view" ?
                         <ImageEmbed images={record?.embeds[0].images} /> : ""}
@@ -195,5 +218,58 @@ const ViewRecord = ({ record }) => {
 }
 
 
+function getFormattedText(post) {
+    let sanitizedContent = sanitizeHtml(post.record.text, {
+        allowedTags: ['b', 'i', 'em', 'strong', 'p', 'br', 'ul', 'li'], // Allowed HTML tags
+        allowedAttributes: {
+            'a': ['href']
+        }
 
+    });
 
+    if (post.record?.facets !== undefined) {
+        post.record?.facets.map((facet, index) => {
+            facet.features.map((feature, index) => {
+                if (feature.$type === "app.bsky.richtext.facet#link") {
+                    const byteStart = facet.index.byteStart;
+                    const byteEnd = facet.index.byteEnd;
+                    const externalLink = `<a style="text-decoration: underline;" href="${feature.uri}" target="_blank" >`;
+
+                    sanitizedContent = sanitizedContent.slice(0, byteStart) + externalLink + sanitizedContent.slice(byteStart, byteEnd) + '</a>' + sanitizedContent.slice(byteEnd);
+
+                }
+            })
+        })
+    }
+    const formattedContent = sanitizedContent.replace(/\n/g, '<br>');
+
+    return formattedContent
+}
+
+function getFormattedTextForRecord(record) {
+    let sanitizedContent = sanitizeHtml(record.value.text, {
+        allowedTags: ['b', 'i', 'em', 'strong', 'p', 'br', 'ul', 'li'], // Allowed HTML tags
+        allowedAttributes: {
+            'a': ['href']
+        }
+
+    });
+
+    if (record?.value?.facets !== undefined) {
+        record?.value?.facets.map((facet, index) => {
+            facet.features.map((feature, index) => {
+                if (feature.$type === "app.bsky.richtext.facet#link") {
+                    const byteStart = facet.index.byteStart;
+                    const byteEnd = facet.index.byteEnd;
+                    const externalLink = `<a style="text-decoration: underline;" href="${feature.uri}" target="_blank" >`;
+
+                    sanitizedContent = sanitizedContent.slice(0, byteStart) + externalLink + sanitizedContent.slice(byteStart, byteEnd) + '</a>' + sanitizedContent.slice(byteEnd);
+
+                }
+            })
+        })
+    }
+    const formattedContent = sanitizedContent.replace(/\n/g, '<br>');
+
+    return formattedContent
+}
